279121929e4ab8e40cac270f5018efc9e0584db5
Lakhan Lothiyi <llsc12.dev@gmail.com>
2025-04-14 10:41:17 +0100

    Add Observation

--- Sources/SwiftTUI/PropertyWrappers/View+ObservableObject.swift
+++ Sources/SwiftTUI/PropertyWrappers/View+ObservableObject.swift
@@ -1,16 +1,18 @@
 #if os(macOS)
-import Foundation
-import Combine
+	import Foundation
+	import Combine
 
-extension View {
-    func setupObservedObjectProperties(node: Node) {
-        for (label, value) in Mirror(reflecting: self).children {
-            if let label, let observedObject = value as? AnyObservedObject {
-                node.subscriptions[label] = observedObject.subscribe {
-                    node.root.application?.invalidateNode(node)
-                }
-            }
-        }
-    }
-}
+	extension View {
+		func setupObservedObjectProperties(node: Node) {
+			for (label, value) in Mirror(reflecting: self).children {
+				if let label, let observedObject = value as? AnyObservedObject {
+					log("subscribing to label: \(label)")
+					node.subscriptions[label] = observedObject.subscribe {
+						log("ObservableObject observed a change. invalidating node...")
+						node.root.application?.invalidateNode(node)
+					}
+				}
+			}
+		}
+	}
 #endif
--- Sources/SwiftTUI/ViewGraph/ComposedView.swift
+++ Sources/SwiftTUI/ViewGraph/ComposedView.swift
@@ -2,25 +2,29 @@ import Foundation
 
 /// This wraps a composed (user-defined) view, so that it can be used in a view graph node.
 struct ComposedView<I: View>: GenericView {
-    let view: I
+	let view: I
 
-    func buildNode(_ node: Node) {
-        view.setupStateProperties(node: node)
-        view.setupEnvironmentProperties(node: node)
-        #if os(macOS)
-        view.setupObservedObjectProperties(node: node)
-        #endif
-        node.addNode(at: 0, Node(view: view.body.view))
-    }
+	func buildNode(_ node: Node) {
+		view.setupStateProperties(node: node)
+		view.setupEnvironmentProperties(node: node)
+		#if os(macOS)
+			view.setupObservedObjectProperties(node: node)
+		#endif
+		if #available(macOS 14.0, *) {
+			view.setupObservableClassProperties(node: node)
+		}
+		node.addNode(at: 0, Node(view: view.body.view))
+	}
 
-    func updateNode(_ node: Node) {
-        view.setupStateProperties(node: node)
-        view.setupEnvironmentProperties(node: node)
-        node.view = self
-        node.children[0].update(using: view.body.view)
-    }
+	func updateNode(_ node: Node) {
+		log("calling updateNode")
+		view.setupStateProperties(node: node)
+		view.setupEnvironmentProperties(node: node)
+		node.view = self
+		node.children[0].update(using: view.body.view)
+	}
 
-    static var size: Int? {
-        I.Body.size
-    }
+	static var size: Int? {
+		I.Body.size
+	}
 }
